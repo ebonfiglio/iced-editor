5a2342b9c74141d912971443a4278ce670e53bda
Evan Bonfiglio <evanbonfiglio@protonmail.com>
2025-11-30 17:46:44 -0500

    Add file path to UI

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use iced::{
     widget::{button, column, container, horizontal_space, row, text, text_editor},
 };
 use smol::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
 pub fn main() -> iced::Result {
@@ -13,6 +13,7 @@ pub fn main() -> iced::Result {
 }
 
 struct Editor {
+    path: Option<PathBuf>,
     content: text_editor::Content,
     error: Option<Error>,
 }
@@ -21,13 +22,11 @@ impl Editor {
     fn new() -> (Self, Task<Message>) {
         (
             Self {
+                path: None,
                 content: text_editor::Content::new(),
                 error: None,
             },
-            Task::perform(
-                load_file(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR"))),
-                Message::FileOpened,
-            ),
+            Task::perform(load_file(default_file()), Message::FileOpened),
         )
     }
 }
@@ -36,7 +35,7 @@ impl Editor {
 enum Message {
     Edit(text_editor::Action),
     Open,
-    FileOpened(Result<Arc<String>, Error>),
+    FileOpened(Result<(PathBuf, Arc<String>), Error>),
 }
 
 impl Editor {
@@ -47,7 +46,8 @@ impl Editor {
                 Task::none()
             }
             Message::Open => Task::perform(pick_file(), Message::FileOpened),
-            Message::FileOpened(Ok(contents)) => {
+            Message::FileOpened(Ok((path, contents))) => {
+                self.path = Some(path);
                 self.content = text_editor::Content::with_text(&contents);
                 Task::none()
             }
@@ -70,7 +70,12 @@ impl Editor {
             text(format!("{}:{}", line + 1, column + 1))
         };
 
-        let status_bar = row![horizontal_space(), position];
+        let file_path = match self.path.as_deref().and_then(Path::to_str) {
+            Some(path) => text(path).size(14),
+            None => text(""),
+        };
+
+        let status_bar = row![file_path, horizontal_space(), position];
 
         container(column![controls, input, status_bar].spacing(10))
             .padding(10)
@@ -82,22 +87,28 @@ impl Editor {
     }
 }
 
-async fn pick_file() -> Result<Arc<String>, Error> {
+async fn pick_file() -> Result<(PathBuf, Arc<String>), Error> {
     let handle = rfd::AsyncFileDialog::new()
         .set_title("Choose a text file...")
         .pick_file()
         .await
         .ok_or(Error::DialogClosed)?;
 
-    load_file(handle.path()).await
+    load_file(handle.path().to_owned()).await
 }
 
-async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, Error> {
-    smol::fs::read_to_string(path)
+async fn load_file(path: PathBuf) -> Result<(PathBuf, Arc<String>), Error> {
+    let contents = smol::fs::read_to_string(&path)
         .await
         .map(Arc::new)
         .map_err(|error| error.kind())
-        .map_err(Error::IO)
+        .map_err(Error::IO)?;
+
+    Ok((path, contents))
+}
+
+fn default_file() -> PathBuf {
+    PathBuf::from(format!("{}/src/main.rs", env!("CARGO_MANIFEST_DIR")))
 }
 
 #[derive(Debug, Clone)]
